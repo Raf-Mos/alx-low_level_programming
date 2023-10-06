2d1ddbb6d3e88602bfe105f4f784aeaa9c0129d4
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-10-06 13:19:17 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print Numbers
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
