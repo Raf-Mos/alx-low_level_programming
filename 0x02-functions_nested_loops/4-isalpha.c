dea1c03017c298ebd4c08b5a2509e967c92473db
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-10-10 14:49:10 -0700

    is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * _isalpha - function if character is a letter
+ *
+ * @c: takes input from other functions
+ *
+ * Return: 1 is c if true else 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
