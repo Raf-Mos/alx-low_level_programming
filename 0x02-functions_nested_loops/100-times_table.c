55df645b0fec5c9d9ac0ac5cbdee03467dab5a87
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-10-10 17:09:28 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,8 +31,7 @@ void print_times_table(int n)
 				{
 					_putchar((k / 100) + 48);
 					_putchar((k / 10) % 10 + 48);
-				}
-				else if (k <= 99 && k >= 10)
+				} else if (k <= 99 && k >= 10)
 					_putchar((k % 10) + 48);
 				_putchar((k % 10) + 48);
 			}
