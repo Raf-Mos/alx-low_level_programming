ea50de7b503bd81247b13c0e50967cb7b08d2420
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-10-12 11:44:25 -0700

    first try

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,19 +15,21 @@ int main(void)
 {
 	int n;
 
-	for (n = 1; n <= 100; n++)
-	{
-		if ((n % 3) == 0 && ((n % 5) == 0)
-			print("FizzBuzz ");
+	printf("1");
 
-		else if ((n % 3) == 0 && ((n % 5) != 0)
-			printf("Fizz ");
+	for (n = 2; n <= 100; n++)
+	{
+		if ((n % 3) == 0 && ((n % 5) != 0)
+			printf(" Fizz");
 
 		else if ((n % 5) == 0 && ((n % 3) != 0)
-			printf("Buzz ");
+			printf(" Buzz");
+
+		else if ((n % 3) == 0 && ((n % 5) == 0)
+			print(" FizzBuzz");
 
 		else
-			printf("%d ", n);
+			printf(" %d", n);
 	}
 	printf("\n");
 }
