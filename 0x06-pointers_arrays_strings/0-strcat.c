86d158bb03377997ce05f97c4fda2494d76500a6
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-10-19 01:27:14 -0700

    first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcat - function that concatenates two strings.
+ *
+ * @dest: string one
+ * @src: string tow
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src);
+{
+    int j,i = 0;
+ 
+    while (dest[i] != '\0')
+        i++;
+    
+    for (j = 0; src[j] != '\0'; j++, i++)
+        dest[i] = src[j];
+        
+    return (dest);
+}
