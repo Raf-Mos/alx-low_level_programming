a6077e1e44adc71a123045425cb6cfe253ad5950
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-10-19 01:51:23 -0700

    file creation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[i] != '\0')
 		i++;
 
-	for (j = 0; j < n; j++, i++)
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
 		dest[i] = src[j];
 
 	return (dest);
