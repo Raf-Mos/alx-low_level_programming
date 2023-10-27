e8231d22cb3de5024b5a11e1b747a124ac20d45b
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-10-27 16:04:32 -0700

    v4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,16 +13,15 @@ int main(int argc, char **argv)
 {
 	int mul;
 
-
-	if (argc < 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	if (argc == 3)
 	{
 		mul = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
