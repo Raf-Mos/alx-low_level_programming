fe81be1bab7067b5d7ead51aec3ca4665fcf7a2e
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-10-27 15:58:04 -0700

    v1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,10 +13,10 @@ int main(int argc, char **argv)
 {
 	if (argc < 3)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
 
-	printf("%s\n", argv[1] * argv[2]);
+	printf("%s\n", *argv[1] * *argv[2]);
 	return (0);
 }
