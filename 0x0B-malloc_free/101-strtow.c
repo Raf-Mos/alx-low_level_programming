a78acfc07939a44f69aade3f7237e89b28243013
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-10-31 15:17:10 -0700

    v1

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,76 @@
+#include "main.h"
+
+/**
+ * wordcnt - counts the number of words in a string
+ * @s: string input
+ * Return: number of words
+ */
+
+int wordcnt(char *s)
+{
+	int i, n = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == ' ')
+		{
+			if (s[i + 1] != ' ' && s[i + 1] != '\0')
+				n++;
+		}
+		else if (i == 0)
+			n++;
+	}
+	n++;
+	return (n);
+}
+
+/**
+ * **strtow - function that splits a string into words
+ *
+ * @str: string input
+ *
+ * Return: pointer
+ */
+
+char **strtow(char *str)
+{
+	int i = 0, j, k, l, n, wc = 0;
+	char **w;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	n = wordcnt(str);
+	if (n == 1)
+		return (NULL);
+	w = (char **)malloc(n * sizeof(char *));
+	if (w == NULL)
+		return (NULL);
+	w[n - 1] = NULL;
+	while (str[i])
+	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
+			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+				;
+			j++;
+			w[wc] = (char *)malloc(j * sizeof(char));
+			j--;
+			if (w[wc] == NULL)
+			{
+				for (k = 0; k < wc; k++)
+					free(w[k]);
+				free(w[n - 1]);
+				free(w);
+				return (NULL);
+			}
+			for (l = 0; l < j; l++)
+				w[wc][l] = str[i + l];
+			w[wc][l] = '\0';
+			wc++;
+			i += j;
+		}
+		else
+			i++;
+	}
+	return (w);
+}
