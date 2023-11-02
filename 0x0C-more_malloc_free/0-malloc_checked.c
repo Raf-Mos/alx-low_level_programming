9320b68028c28da3442eecd87632f0d1a622030d
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-11-02 16:27:26 -0700

    dir creation

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc.
+ *
+ * @b: input
+ *
+ * Return: pointer to the initialized array or NULL
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *m = malloc(b);
+
+	if (m == 0)
+		exit(98);
+
+	return (m);
+}
