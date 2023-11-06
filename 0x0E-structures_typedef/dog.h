70af6fccaa8c2205e487aa5bf56b5be25267eabc
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-11-06 15:52:21 -0800

    create dir

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new data type
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog onwer's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
