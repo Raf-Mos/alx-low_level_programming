b1ed5c2d76936868b464786b9a2d199bfe014783
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-11-09 15:51:21 -0800

    v2

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,22 +16,14 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	unsigned int i;
 
-	if (n)
-	{
-	if (n <= 0)
-	{
+	if (n == 0)
 		return (0);
-	}
 
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
-	{
 		sum += va_arg(ap, int);
-	}
 
 	va_end(ap);
 	return (sum);
-	}
-	return (0);
 }
