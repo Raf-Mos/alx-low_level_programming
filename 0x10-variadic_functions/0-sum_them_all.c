9874bc32a7659e70e9c0aa6cc546ea4e9944a6e0
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-11-09 15:45:16 -0800

    v8

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,13 +18,17 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n)
 	{
-	if (n == 0)
+	if (n <= 0)
+	{
 		return (0);
+	}
 
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
+	{
 		sum += va_arg(ap, int);
+	}
 
 	va_end(ap);
 	return (sum);
