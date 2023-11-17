acf1f384fdb4c4094d48b1c4b224d4a2e738ab81
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-11-16 16:12:13 -0800

    v4

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,17 +11,27 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *last_node = malloc(sizeof(list_t));
-
+	list_t *link = *head;
 
 	if (!head || !last_node)
 		return (NULL);
-
+	if (str)
+	{
 		last_node->str = strdup(str);
-		list_t *link = *head;
-
+		if (!last_node->str)
+		{
+			free(last_node);
+			return (NULL);
+		}
+		last_node->len = _strlen(last_node->str);
+	}
+	if (node)
+	{
 		while (link->next)
 			link = link->next;
-
 		link->next = last_node;
+	}
+	else
+		*head = last_node;
 	return (last_node);
 }
