ba8d6be173022b74c8c2b5a1ef06d2ed3e4086e9
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-11-22 14:01:03 -0800

    v3

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,27 +9,25 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *node1 = head, *node2;
+	const listint_t *n1 = head;
 	size_t i = 1;
 
-	while (node1)
+	printf("[%p] %d\n", (void *)n1, n1->n);
+
+	n1 = n1->next;
+
+	while (n1)
 	{
-		printf("[%p] %d\n", (void *)node1, node1->n);
+		printf("[%p] %d\n", (void *)n1, n1->n);
 		i++;
 
-		node2 = head;
-
-		while (node2 != node1)
+		if (n1 <= n1->next)
 		{
-			if (node2 == node1->next)
-			{
-				printf("-> [%p] %d\n", (void *)node1->next, node1->next->n);
-				exit(98);
-			}
-			node2 = node2->next;
+			printf("-> [%p] %d\n", (void *)n1->next, n1->next->n);
+			exit(98);
 		}
-		node1 = node1->next;
-	}
 
+		n1 = n1->next;
+	}
 	return (i);
 }
