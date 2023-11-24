0abfe86539229ffb15407531b0b165710bf98234
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-11-23 17:17:53 -0800

    Mandatory tasks

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - return number of bits that would need
+ *  to be flipped to transform one number to another
+ *
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to flip
+ */
+ 
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor_v = n ^ m;
+	unsigned int i = 0;
+
+	while (xor_v)
+	{
+		if (xor_v & 1ul)
+			i++;
+		xor_v = xor_v >> 1;
+	}
+	return (i);
+}
