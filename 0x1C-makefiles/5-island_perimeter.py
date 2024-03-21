4e55284e6c70ea09ec46ecd3771851fb1169d995
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-03-21 00:46:06 +0000

    Add Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Island perimeter calculator"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
+    x = len(grid[0])
+    y = len(grid)
+    land = 0
+    edge = 0
+
+    for i in range(y):
+        for j in range(x):
+            if grid[i][j] == 1:
+                land += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edge += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edge += 1
+
+    return land * 4 - edge * 2
